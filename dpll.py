96beecdc05630dcd805f9371057d4c517293a9eb
uekann <kantaueno0808@yahoo.co.jp>
2023-06-18 02:48:56 +0900

    Value型の追加、式の評価の追加

--- dpll.py
+++ dpll.py
@@ -1,6 +1,33 @@
 from __future__ import annotations
 
 from dataclasses import dataclass
+from enum import Enum
+
+
+class Value(Enum):
+    UD = 0
+    T = 1
+    F = 2
+
+    def __str__(self) -> str:
+        if self == Value.UD:
+            return "UNDEFINED"
+        elif self == Value.T:
+            return "TRUE"
+        elif self == Value.F:
+            return "FALSE"
+        else:
+            raise ValueError("Value must be UNDEFINED, TRUE or FALSE")
+
+    def __invert__(self) -> Value:
+        if self == Value.UD:
+            return Value.UD
+        elif self == Value.T:
+            return Value.F
+        elif self == Value.F:
+            return Value.T
+        else:
+            raise ValueError("Value must be UNDEFINED, TRUE or FALSE")
 
 
 class CNF:
@@ -29,7 +56,7 @@ class Section(CNF):
         sign: bool = True
 
         def __str__(self) -> str:
-            return f"(-{str(self.var)})" if not self.sign else str(self.var)
+            return f"(~{str(self.var)})" if not self.sign else str(self.var)
 
         def __eq__(self, __value: object) -> bool:
             if isinstance(__value, Section.VariableWithSign):
@@ -38,6 +65,9 @@ class Section(CNF):
 
         def __hash__(self) -> int:
             return hash((self.var, self.sign))
+
+        def evaluate(self) -> Value:
+            return self.var.value if self.sign else ~self.var.value
 
     def __init__(self, v: Variable = None) -> None:
         super().__init__()
@@ -54,6 +84,20 @@ class Section(CNF):
     def __str__(self) -> str:
         return "(" + " + ".join(map(str, self.set)) + ")"
 
+    def evaluate(self) -> Value:
+        if len(self.set) == 0:
+            return Value.T
+        else:
+            for vws in self.set:
+                flag = False
+                if vws.evaluate() == Value.T:
+                    return Value.T
+
+                if vws.evaluate() == Value.UD:
+                    flag = True
+
+            return Value.UD if flag else Value.F
+
 
 class Variable(Section):
     def __init__(self, name):
@@ -62,16 +106,22 @@ class Variable(Section):
         vws = Section.VariableWithSign(self)
         self.set = {vws}
         self.name = name
+        self.value = Value.UD
 
     def __str__(self) -> str:
         return self.name
 
-    def __neg__(self) -> Section.VariableWithSign:
+    def __invert__(self) -> Section.VariableWithSign:
         s = Section()
         s.set.add(Section.VariableWithSign(self, False))
         return s
 
-    # def _set(self, value):
+    def _set_value(self, value: Value):
+        if not isinstance(value, Value):
+            raise ValueError(
+                "Value must be UNDEFINED, TRUE or FALSE, but got " + str(value)
+            )
+        self.value = value
 
 
 if __name__ == "__main__":
@@ -79,4 +129,4 @@ if __name__ == "__main__":
     b = Variable("b")
     c = Variable("c")
 
-    print((a + (-b)) * c * (b + c))
+    print((a + (~b)) * c * (b + c))
