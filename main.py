c35b073878f0143cd95a706d669954e23dbc65ae
uekann <kantaueno0808@yahoo.co.jp>
2023-06-18 02:49:26 +0900

    反転を__neg__から__invert__に変更

--- main.py
+++ main.py
@@ -4,4 +4,4 @@ a = Variable("a")
 b = Variable("b")
 c = Variable("c")
 
-print((a + a + (-a) + (-b)) * c * (b + c))  # (a + (-b)) * c * (b + c)
+print((a + a + (~a) + (~b)) * c * (b + c))  # (a + (~b)) * c * (b + c)
